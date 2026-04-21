ef400d642c3ca2d21ab737c9a872145a5a2b1f7f
aminokasdev <sagyntayamina@gmail.com>
2026-04-21 23:04:27 +0500

    первая попытка

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,18 +1,38 @@
 import {getPages} from "../lib/utils.js";
 
 export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) => {
-    // @todo: #2.3 — подготовить шаблон кнопки для страницы и очистить контейнер
+    // #2.3 — подготовить шаблон кнопки (ДО return!)
+    const pageTemplate = pages.firstElementChild.cloneNode(true);
+    pages.firstElementChild.remove();
 
     return (data, state, action) => {
-        // @todo: #2.1 — посчитать количество страниц, объявить переменные и константы
+        // #2.1
+        const rowsPerPage = state.rowsPerPage;
+        const pageCount = Math.ceil(data.length / rowsPerPage);
+        let page = state.page;
 
-        // @todo: #2.6 — обработать действия
+        // #2.6
+        if (action) switch(action.name) {
+            case 'prev': page = Math.max(1, page - 1); break;
+            case 'next': page = Math.min(pageCount, page + 1); break;
+            case 'first': page = 1; break;
+            case 'last': page = pageCount; break;
+        }
 
-        // @todo: #2.4 — получить список видимых страниц и вывести их
+        // #2.4
+        const visiblePages = getPages(page, pageCount, 5);
+        pages.replaceChildren(...visiblePages.map(pageNumber => {
+            const el = pageTemplate.cloneNode(true);
+            return createPage(el, pageNumber, pageNumber === page);
+        }));
 
-        // @todo: #2.5 — обновить статус пагинации
+        // #2.5
+        fromRow.textContent = (page - 1) * rowsPerPage + 1;
+        toRow.textContent = Math.min(page * rowsPerPage, data.length);
+        totalRows.textContent = data.length;
 
-        // @todo: #2.2 — посчитать сколько строк нужно пропустить и получить срез данных
-        return data.slice(0, 10);
+        // #2.2
+        const skip = (page - 1) * rowsPerPage;
+        return data.slice(skip, skip + rowsPerPage);
     }
 }
